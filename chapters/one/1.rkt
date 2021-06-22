5242dbfe7249a4f1db20434dc9f59bb4b4f63767
zhd4nov <zhdanov@element-soft.com>
2021-06-22 23:15:18 +0300

    touch racket lang, check env

--- chapters/one/1.rkt
+++ chapters/one/1.rkt
@@ -2,7 +2,28 @@
 
 (#%require rackunit)
 
-(define solution
-   (+ 2 2))
+;check env
+(define (square x) (* x x))
 
-(check-equal? solution 4)
+(define answer (square 2))
+
+(check-equal? answer 4)
+
+(define (sum-of-square x y)
+  (+ (square x) (square y)))
+
+(check-equal? (sum-of-square 5 10) 125)
+
+(define (abs x)
+  (cond ((< x 0) (- x))
+        (else x)))
+
+(check-equal? (abs -1) 1)
+(check-equal? (abs 1) 1)
+(check-equal? (abs 0) 0)
+
+(define (>= x y) (if (< x y) #f #t))
+
+(check-true (>= 2 0))
+(check-true (>= 0 0))
+(check-false (>= (- 2) 0))
