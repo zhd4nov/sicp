34988cdd665657a3ac7a6ade100691101ea9f8aa
zhd4nov <zhdanov@element-soft.com>
2021-07-06 21:45:08 +0300

    fix 1.15 unbound identifier

--- chapters/one/1.15.rkt
+++ chapters/one/1.15.rkt
@@ -1,16 +1,16 @@
 #lang sicp
 
-(#%require "../../utils/utils.rkt")
-
 ; The sine of an angle (specified in radians) can be computed by making use of the approximation 
 ; sin x = x if x is sufficiently small, and the trigonometric identity
 
-(define (p x) (- (* 3 x) (* 4 (cube x))))
+(define (сube x) (* x x x))
+
+(define (p x) (- (* 3 x) (* 4 (сube x))))
 
 (define (sine angle)
-   (if (not (> (abs angle) 0.1))
-       angle
-       (p (sine (/ angle 3.0)))))
+  (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
 
 ; a. How many times is the procedure p applied when (sine 12.15) is evaluated?
 
