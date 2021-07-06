bdb0259b0ab2e276109567527edc6e73eb15fc26
zhd4nov <zhdanov@element-soft.com>
2021-07-06 21:30:20 +0300

    add 1.13 description

---
+++ chapters/one/1.13.rkt
@@ -0,0 +1,7 @@
+#lang sicp
+
+; Prove that Fib(n) is the closest integer to ϕn/√5, where ϕ = (1 + √5)/2. Hint: Let ψ = (1 - √5)/2.
+; Use induction and the definition of the Fibonacci numbers (see section 1.2.2) to prove
+; that Fib(n) = (ϕn - ψn)/√5.
+
+; Math is gorgeous: skip
